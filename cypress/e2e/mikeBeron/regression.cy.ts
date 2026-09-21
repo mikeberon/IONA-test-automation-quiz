30a2d096d44f89c93315dcc206595ee45b051f7d
Mike <mikeandrew.beron@gmail.com>
2026-09-21 21:47:48 +0800

    Add regression coverage

---
+++ cypress/e2e/mikeBeron/regression.cy.ts
@@ -0,0 +1,83 @@
+describe('DemoBlaze - Regression', () => {
+
+    const productName = 'Samsung galaxy s6'
+    const productPrice = 360
+
+    beforeEach(() => {
+        cy.visit('/')
+    })
+
+    it('should display the correct product details', () => {
+        cy.contains('.hrefch', productName)
+            .should('be.visible')
+            .click()
+
+        cy.get('.name')
+            .should('be.visible')
+            .and('have.text', productName)
+
+        cy.get('.price-container')
+            .should('be.visible')
+            .and('contain.text', `$${productPrice}`)
+    })
+
+    it('should add a product to the cart and display the correct total', () => {
+        cy.intercept('POST', '**/addtocart').as('addToCart')
+
+        cy.contains('.hrefch', productName)
+            .should('be.visible')
+            .click()
+
+        cy.contains('a', 'Add to cart')
+            .should('be.visible')
+            .click()
+
+        cy.wait('@addToCart')
+            .its('response.statusCode')
+            .should('eq', 200)
+
+        cy.get('#cartur')
+            .should('be.visible')
+            .click()
+
+        cy.contains('#tbodyid td', productName)
+            .should('be.visible')
+
+        cy.get('#totalp')
+            .should('be.visible')
+            .and('have.text', productPrice.toString())
+    })
+
+    it('should remove a product from the cart', () => {
+        cy.intercept('POST', '**/addtocart').as('addToCart')
+
+        cy.contains('.hrefch', productName)
+            .should('be.visible')
+            .click()
+
+        cy.contains('a', 'Add to cart')
+            .should('be.visible')
+            .click()
+
+        cy.wait('@addToCart')
+            .its('response.statusCode')
+            .should('eq', 200)
+
+        cy.get('#cartur')
+            .should('be.visible')
+            .click()
+
+        cy.contains('#tbodyid td', productName)
+            .should('be.visible')
+
+        cy.contains('#tbodyid tr', productName)
+            .within(() => {
+                cy.contains('a', 'Delete')
+                    .should('be.visible')
+                    .click()
+            })
+
+        cy.contains('#tbodyid td', productName)
+            .should('not.exist')
+    })
+})
